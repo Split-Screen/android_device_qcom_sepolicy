a62af25943fea8e801a4108bcd78f21b0b18b609
Steve Kondik <steve@cyngn.com>
2015-10-31 06:33:29 -0700

    sepolicy: Include PAC+QCOM SEpolicy
    Change-Id: I6b7f1716f9850e93145d9d379df1ffc0d97e19a2

--- sepolicy.mk
+++ sepolicy.mk
@@ -5,3 +5,5 @@ BOARD_SEPOLICY_DIRS := \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+
+-include vendor/pac/sepolicy/qcom/sepolicy.mk
