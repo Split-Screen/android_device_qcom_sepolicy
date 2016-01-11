d9e6c6c622f13c0b38428ab2f9204204ad644d5c
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2016-01-11 07:33:52 -0500

    Sepolicy: Changes for caf branch
    Change-Id: I0eec13d5c57246296698c494a57e5dba011afc01
    Signed-off-by: Lokesh Chamane <lokesh.chamane@pac-rom.com>

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,9 +1,9 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+       device/qcom/sepolicy-caf \
+       device/qcom/sepolicy-caf/common \
+       device/qcom/sepolicy-caf/test \
+       device/qcom/sepolicy-caf/$(TARGET_BOARD_PLATFORM)
 
 -include vendor/pac/sepolicy/qcom/sepolicy.mk
